9bed73f6933b877e6525994e080605149ccf9ed2
SSowmya7 <ssowmya@qualminds.com>
2024-08-05 17:54:24 +0530

    written unit test cases at controller,services level for menu settings

---
+++ Test/Tests/MenuSettingsControllerTests.cs
@@ -0,0 +1,196 @@
+﻿using AutoMapper;
+using CaseStudy.Application.VM;
+using CaseStudy.Core.Contracts.IUnitOfWork;
+using CaseStudy.Core.DTO;
+using CaseStudy.Core.Models;
+using CaseStudy.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class MenuSettingsControllerTests
+    {
+        private readonly Mock<IMenuSettingsServices> _mockMenuSettingsServices;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly MenuSettingsController _controller;
+
+        public MenuSettingsControllerTests()
+        {
+            _mockMenuSettingsServices = new Mock<IMenuSettingsServices>();
+            _mockMapper = new Mock<IMapper>();
+            _controller = new MenuSettingsController(_mockMenuSettingsServices.Object, _mockMapper.Object);
+        }
+
+        [Fact]
+        public async Task GetMenuSettings_ShouldReturnOkResultWithMenuSettings()
+        {
+            // Arrange
+            var menuSettings = new List<MenuSettings>
+            {
+                new MenuSettings { DealerId = 1, MenuType = "Type1" },
+                new MenuSettings { DealerId = 2, MenuType = "Type2" }
+            };
+            _mockMenuSettingsServices.Setup(service => service.GetMenuSettings()).ReturnsAsync(menuSettings);
+
+            // Act
+            var result = await _controller.GetMenuSettings();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsType<List<MenuSettings>>(okResult.Value);
+            Assert.Equal(2, returnValue.Count);
+            _mockMenuSettingsServices.Verify(service => service.GetMenuSettings(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetMenuSettings_ShouldReturnNoContentWhenNoMenuSettings()
+        {
+            // Arrange
+            _mockMenuSettingsServices.Setup(service => service.GetMenuSettings()).ReturnsAsync((IEnumerable<MenuSettings>)null);
+
+            // Act
+            var result = await _controller.GetMenuSettings();
+
+            // Assert
+            Assert.IsType<NoContentResult>(result.Result);
+            _mockMenuSettingsServices.Verify(service => service.GetMenuSettings(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetMenuSettingsById_ShouldReturnOkResultWithMenuSettings()
+        {
+            // Arrange
+            var menuSettings = new MenuSettings { DealerId = 1, MenuType = "Type1" };
+            _mockMenuSettingsServices.Setup(service => service.GetMenuSettingsById(1)).ReturnsAsync(menuSettings);
+
+            // Act
+            var result = await _controller.GetMenuSettingsById(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsType<MenuSettings>(okResult.Value);
+            Assert.Equal(1, returnValue.DealerId);
+            _mockMenuSettingsServices.Verify(service => service.GetMenuSettingsById(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetMenuSettingsById_ShouldReturnNotFoundWhenMenuSettingsNotFound()
+        {
+            // Arrange
+            _mockMenuSettingsServices.Setup(service => service.GetMenuSettingsById(1)).ReturnsAsync((MenuSettings)null);
+
+            // Act
+            var result = await _controller.GetMenuSettingsById(1);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Equal("Dealer Not Found", notFoundResult.Value);
+            _mockMenuSettingsServices.Verify(service => service.GetMenuSettingsById(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddMenuSettings_ShouldReturnOkResultWhenMenuSettingsAdded()
+        {
+            // Arrange
+            var menuSettings = new MenuSettings { DealerId = 1, MenuType = "Type1" };
+            _mockMenuSettingsServices.Setup(service => service.AddMenuSettings(menuSettings)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.AddMenuSettings(menuSettings);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsType<MenuSettings>(okResult.Value);
+            Assert.Equal(1, returnValue.DealerId);
+            _mockMenuSettingsServices.Verify(service => service.AddMenuSettings(menuSettings), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddMenuSettings_ShouldReturnNotFoundWhenDealerNotFound()
+        {
+            // Arrange
+            var menuSettings = new MenuSettings { DealerId = 1, MenuType = "Type1" };
+            _mockMenuSettingsServices.Setup(service => service.AddMenuSettings(menuSettings)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.AddMenuSettings(menuSettings);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Equal("Dealer Not Found", notFoundResult.Value);
+            _mockMenuSettingsServices.Verify(service => service.AddMenuSettings(menuSettings), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateMenuSettings_ShouldReturnOkResultWhenMenuSettingsUpdated()
+        {
+            // Arrange
+            var menuSettingsVM = new MenuSettingsVM { DealerId = 1, MenuType = "Type2", SrpFilterPosition = "Right" };
+            var menuSettingsDTO = new MenuSettingsDTO { DealerId = 1, MenuType = "Type2", SrpFilterPosition = "Right" };
+            _mockMapper.Setup(m => m.Map<MenuSettingsVM, MenuSettingsDTO>(menuSettingsVM)).Returns(menuSettingsDTO);
+            _mockMenuSettingsServices.Setup(service => service.UpdateMenuSettings(menuSettingsDTO)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.UpdateMenuSettings(1, menuSettingsVM);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsType<MenuSettingsVM>(okResult.Value);
+            Assert.Equal("Type2", returnValue.MenuType);
+            _mockMenuSettingsServices.Verify(service => service.UpdateMenuSettings(menuSettingsDTO), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateMenuSettings_ShouldReturnNotFoundWhenMenuSettingsNotFound()
+        {
+            // Arrange
+            var menuSettingsVM = new MenuSettingsVM { DealerId = 1, MenuType = "Type2", SrpFilterPosition = "Right" };
+            var menuSettingsDTO = new MenuSettingsDTO { DealerId = 1, MenuType = "Type2", SrpFilterPosition = "Right" };
+            _mockMapper.Setup(m => m.Map<MenuSettingsVM, MenuSettingsDTO>(menuSettingsVM)).Returns(menuSettingsDTO);
+            _mockMenuSettingsServices.Setup(service => service.UpdateMenuSettings(menuSettingsDTO)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.UpdateMenuSettings(1, menuSettingsVM);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result.Result);
+            Assert.Equal("Record Not Found", notFoundResult.Value);
+            _mockMenuSettingsServices.Verify(service => service.UpdateMenuSettings(menuSettingsDTO), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteMenuSettings_ShouldReturnOkResultWhenMenuSettingsDeleted()
+        {
+            // Arrange
+            _mockMenuSettingsServices.Setup(service => service.DeleteMenuSettings(1)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteMenuSettings(1);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<string>(okResult.Value);
+            Assert.Equal("Record with DealerId:- 1 Successfully deleted", returnValue);
+            _mockMenuSettingsServices.Verify(service => service.DeleteMenuSettings(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteMenuSettings_ShouldReturnNotFoundWhenMenuSettingsNotFound()
+        {
+            // Arrange
+            _mockMenuSettingsServices.Setup(service => service.DeleteMenuSettings(1)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.DeleteMenuSettings(1);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("Record Not Found", notFoundResult.Value);
+            _mockMenuSettingsServices.Verify(service => service.DeleteMenuSettings(1), Times.Once);
+        }
+    }
+}
---
+++ Test/Tests/MenuSettingsServicesTests.cs
@@ -0,0 +1,162 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CaseStudy.Core.Contracts.IReposritories;
+using CaseStudy.Core.DTO;
+using CaseStudy.Core.Models;
+using CaseStudy.Infrastructure.UnitOfWork;
+using Moq;
+using Xunit;
+
+namespace Tests
+{
+    public class MenuSettingsServicesTests
+    {
+        private readonly Mock<IMenuSettingsRepo> _mockRepo;
+        private readonly MenuSettingsServices _service;
+
+        public MenuSettingsServicesTests()
+        {
+            _mockRepo = new Mock<IMenuSettingsRepo>();
+            _service = new MenuSettingsServices(_mockRepo.Object);
+        }
+
+        [Fact]
+        public async Task GetMenuSettings_ShouldReturnAllMenuSettings()
+        {
+            // Arrange
+            var menuSettings = new List<MenuSettings>
+            {
+                new MenuSettings { DealerId = 1, MenuType = "Type1" },
+                new MenuSettings { DealerId = 2, MenuType = "Type2" }
+            };
+            _mockRepo.Setup(repo => repo.GetMenuSettings()).ReturnsAsync(menuSettings);
+
+            // Act
+            var result = await _service.GetMenuSettings();
+
+            // Assert
+            Assert.Equal(2, result.Count());
+            _mockRepo.Verify(repo => repo.GetMenuSettings(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetMenuSettingsById_ShouldReturnMenuSettings()
+        {
+            // Arrange
+            var menuSettings = new MenuSettings { DealerId = 1, MenuType = "Type1" };
+            _mockRepo.Setup(repo => repo.GetMenuSettingsById(1)).ReturnsAsync(menuSettings);
+
+            // Act
+            var result = await _service.GetMenuSettingsById(1);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Type1", result.MenuType);
+            _mockRepo.Verify(repo => repo.GetMenuSettingsById(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetMenuSettingsById_ShouldReturnNullIfNotFound()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.GetMenuSettingsById(1)).ReturnsAsync((MenuSettings)null);
+
+            // Act
+            var result = await _service.GetMenuSettingsById(1);
+
+            // Assert
+            Assert.Null(result);
+            _mockRepo.Verify(repo => repo.GetMenuSettingsById(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddMenuSettings_ShouldAddMenuSettings()
+        {
+            // Arrange
+            var menuSettings = new MenuSettings { DealerId = 1, MenuType = "Type1" };
+            _mockRepo.Setup(repo => repo.AddMenuSettings(menuSettings)).ReturnsAsync(true);
+
+            // Act
+            var result = await _service.AddMenuSettings(menuSettings);
+
+            // Assert
+            Assert.True(result);
+            _mockRepo.Verify(repo => repo.AddMenuSettings(menuSettings), Times.Once);
+        }
+
+        [Fact]
+        public async Task AddMenuSettings_ShouldReturnFalseIfNotAdded()
+        {
+            // Arrange
+            var menuSettings = new MenuSettings { DealerId = 1, MenuType = "Type1" };
+            _mockRepo.Setup(repo => repo.AddMenuSettings(menuSettings)).ReturnsAsync(false);
+
+            // Act
+            var result = await _service.AddMenuSettings(menuSettings);
+
+            // Assert
+            Assert.False(result);
+            _mockRepo.Verify(repo => repo.AddMenuSettings(menuSettings), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateMenuSettings_ShouldUpdateMenuSettings()
+        {
+            // Arrange
+            var menuSettingsDto = new MenuSettingsDTO { DealerId = 1, MenuType = "Type2", SrpFilterPosition = "Right" };
+            _mockRepo.Setup(repo => repo.UpdateMenuSettings(menuSettingsDto)).ReturnsAsync(true);
+
+            // Act
+            var result = await _service.UpdateMenuSettings(menuSettingsDto);
+
+            // Assert
+            Assert.True(result);
+            _mockRepo.Verify(repo => repo.UpdateMenuSettings(menuSettingsDto), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateMenuSettings_ShouldReturnFalseIfNotUpdated()
+        {
+            // Arrange
+            var menuSettingsDto = new MenuSettingsDTO { DealerId = 1, MenuType = "Type2", SrpFilterPosition = "Right" };
+            _mockRepo.Setup(repo => repo.UpdateMenuSettings(menuSettingsDto)).ReturnsAsync(false);
+
+            // Act
+            var result = await _service.UpdateMenuSettings(menuSettingsDto);
+
+            // Assert
+            Assert.False(result);
+            _mockRepo.Verify(repo => repo.UpdateMenuSettings(menuSettingsDto), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteMenuSettings_ShouldDeleteMenuSettings()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.DeleteMenuSettings(1)).ReturnsAsync(true);
+
+            // Act
+            var result = await _service.DeleteMenuSettings(1);
+
+            // Assert
+            Assert.True(result);
+            _mockRepo.Verify(repo => repo.DeleteMenuSettings(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteMenuSettings_ShouldReturnFalseIfNotDeleted()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.DeleteMenuSettings(1)).ReturnsAsync(false);
+
+            // Act
+            var result = await _service.DeleteMenuSettings(1);
+
+            // Assert
+            Assert.False(result);
+            _mockRepo.Verify(repo => repo.DeleteMenuSettings(1), Times.Once);
+        }
+    }
+}
